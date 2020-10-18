81bc90a2e8f38cc5a75bc7801dc8ef5d09f6aa78
Mateusz Choma <kontakt@coderoad.pl>
2020-10-18 15:34:56 +0200

    big sync tasks - blocking main thread

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ const getName = () => 'Mateusz'
 const getLastName = () => 'Choma'
 
 console.log(sayHello())
+
+console.time()
+// const length = 1000 * 1000 * 1000
+const length = 10 * 1000 * 1000
+for (let i = 0; i < length; i++) {
+    if (i === (length - 1)) console.log(i + 1)
+}
+console.timeEnd()
